a48b7eb49135b71df4a40549cbec91b5dc8132d7
rileykacmarynski <riley.kacmarynski@gmail.com>
2018-03-01 19:43:48 -0600

    started working on client

--- chatql-client/src/index.js
+++ chatql-client/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, split } from 'apollo-link-ws';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+import * as constants from './constants';
+
+const httpLink = new HttpLink({uri: appSettings.URI});
+
+const middlewareAuthLink = new ApolloLink((operation, next) => {
+    const token = localStorage.getItem(constants.AUTH_TOKEN);
+    const authorizationHeader = token ? `Bearer ${token}` : null
+    operation.setContext({
+        headers: {
+            authorization: authorizationHeader
+        }
+    });
+    return next(operation);
+})
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
