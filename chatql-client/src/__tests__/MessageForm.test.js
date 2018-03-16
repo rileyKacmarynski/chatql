1ed0892386a8e297c56e965847eabd13b33a19bd
rileykacmarynski <riley.kacmarynski@gmail.com>
2018-03-15 21:12:21 -0500

    Finished testing messenger, going to TDD from here on out (maybe?)

---
+++ chatql-client/src/__tests__/MessageForm.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+
+import MessageForm from '../containers/Messenger/MessageForm';
+
+configure({adapter: new Adapter() });
+
+describe('<MessageForm />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<MessageForm />);
+  });
+
+  
+})
