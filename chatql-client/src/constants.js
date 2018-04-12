d32d2233c2f91c1a21ef645bc06073af1d5e0b29
rileykacmarynski <riley.kacmarynski@gmail.com>
2018-04-12 18:01:08 -0500

    Changed uri's to use azure server.

--- chatql-client/src/constants.js
+++ chatql-client/src/constants.js
@@ -1,3 +1,3 @@
-export const URI = 'http://localhost:4000';
+export const URI = 'https://chatqldemoclient.azurewebsites.net';
 export const AUTH_TOKEN = 'AUTH_TOKEN';
-export const WS_LINK = 'ws://localhost:4000';
+export const WS_LINK = 'ws://chatqldemoclient.azurewebsites.net';
