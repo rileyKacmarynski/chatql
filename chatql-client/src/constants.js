c58054fcc06a21f56eba1b7aaad190fcfc0fe5e9
rileykacmarynski <riley.kacmarynski@gmail.com>
2018-04-12 18:01:40 -0500

    changed client constants to azure server.

--- chatql-client/src/constants.js
+++ chatql-client/src/constants.js
@@ -1,3 +1,3 @@
-export const URI = 'https://chatqldemoclient.azurewebsites.net';
+export const URI = 'https://chatqldemoserver.azurewebsites.net';
 export const AUTH_TOKEN = 'AUTH_TOKEN';
-export const WS_LINK = 'ws://chatqldemoclient.azurewebsites.net';
+export const WS_LINK = 'ws://chatqldemoserver.azurewebsites.net';
