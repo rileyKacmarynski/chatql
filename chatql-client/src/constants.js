b1e04fb57a717079756b9209a7fbcf05a5495eaa
rileykacmarynski <riley.kacmarynski@gmail.com>
2018-04-12 21:10:00 -0500

    App service was a pain. Trying Heroku.

--- chatql-client/src/constants.js
+++ chatql-client/src/constants.js
@@ -1,3 +1,3 @@
-export const URI = 'https://chatqldemoserver.azurewebsites.net';
+export const URI = 'https://ancient-cliffs-99593.herokuapp.com';
 export const AUTH_TOKEN = 'AUTH_TOKEN';
-export const WS_LINK = 'ws://chatqldemoserver.azurewebsites.net';
+export const WS_LINK = 'wss://ancient-cliffs-99593.herokuapp.com';
